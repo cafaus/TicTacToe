a2af14571720fb4dee8537a846a4bdb42171d215
Calvin Faustine <cafaus.05@gmail.com>
2020-09-29 22:11:02 +0700

    Updates

---
+++ precache-manifest.7eb6325e8906c3102b8d318ea2a4454c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4baac656c7ddfa16f2ae86262a1f9ff3",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "3e6a8a2a4a6b6bfc84b1",
+    "url": "/TicTacToe/static/css/main.18522824.chunk.css"
+  },
+  {
+    "revision": "6b5fd48b501dac476cc9",
+    "url": "/TicTacToe/static/js/2.87983f15.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TicTacToe/static/js/2.87983f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6a8a2a4a6b6bfc84b1",
+    "url": "/TicTacToe/static/js/main.f3546f80.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  }
+]);
